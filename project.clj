4df1e7201e7e8260f486fc3730979cd588940c30
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-07-09 01:02:04 +0200

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-arrow "1.0.1-SNAPSHOT"
+(defproject clj-arrow "1.0.2-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.logging "0.1.2"]])
